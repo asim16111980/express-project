ca1e3392367eedaabca1253b45984d8191dfa398
عاصم محمود <asim16111980@gmail.com>
2025-05-06 22:23:33 +0200

    update

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ import mongoose from "mongoose";
 import productsRouter from "./routes/products.route.js";
 import "dotenv/config";
 import httpStatusText from "./utils/httpStatusText.js";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use("/api/products", productsRouter);
-app.all("*", (req, res) => {
+app.all("*any-route", (req, res) => {
   return res.status(404).json({
     status: httpStatusText.ERROR,
     message: "This resource is not available",
