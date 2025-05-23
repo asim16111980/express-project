484b425711cc2d758a303013ebbfc45acce4ce95
عاصم محمود <asim16111980@gmail.com>
2025-05-23 22:27:51 +0200

    update

--- routes/users.route.js
+++ routes/users.route.js
@@ -4,12 +4,24 @@ import { userValidation } from "../middlewares/validationSchema.js";
 import verifyToken from "../middlewares/verifyToken.js";
 import multer from "multer";
 
-const upload = multer({ dest: "uploads/" });
+const diskStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (req, file, cb) => {
+    const ext = file.mimetype.split("/")[1];
+    const fileName = `user-${Date.now()}.${ext}`;
+    cb(null, fileName);
+  },
+});
+const upload = multer({ storage: diskStorage });
 const router = express.Router();
 
 router.route("/").get(verifyToken, usersController.getAllUsers);
 
-router.route("/register").post(usersController.register);
+router
+  .route("/register")
+  .post(upload.single("avatar"), usersController.register);
 
 router.route("/login").post(userValidation(), usersController.login);
 
